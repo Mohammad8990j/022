c5880f1c7e2bc885ac77300b01f9fee1ffb42fb8
Mohammad8990j <mjafary732@gmail.com>
2025-04-20 00:10:14 +0330

    Delete src directory

--- src/connectors/__init__.py
+++
@@ -1,4 +0,0 @@
-from .mt5_connector import MT5Connector
-from .data_fetcher import DataFetcher
-
-__all__ = ['MT5Connector', 'DataFetcher']
--- src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .logger import TradingBotLogger
-from .config_loader import ConfigLoader
-from .error_handler import retry, safe_execute
-
-__all__ = ['TradingBotLogger', 'ConfigLoader', 'retry', 'safe_execute']
